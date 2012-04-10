922c0c9bdd65b2d95eeb9973bb5e93651e271bdc
Mickael Kerbrat <stolati@gmail.com>
2012-04-10 22:57:38 +0200

    doing import rework

--- script2script/lang/python/ast2simple/transform/moreImport.py
+++ script2script/lang/python/ast2simple/transform/moreImport.py
@@ -22,260 +22,399 @@ from nodeTransformer import str2ast
 #   X = import('X')
 #   X = __import__('X')
 
+#  from . import echo
+#  from .. import formats
+#  from ..filters import equalizer
+
 from script2script.lang.python.ast2simple.parsePython import ast2str
 
 
-class MoreImport(nodeTransformer.NodeTransformer):
+#class MoreImport(nodeTransformer.NodeTransformer):
+#
+#  def __init__(self, path=sys.path):
+#    self.path = path
+#
+#  def visit(self, node):
+#    res = NoMoreImport(self.path).visit_withAdd(node)
+#    print ast2str(res, 'pyAst_python')
+#    return res
+#
+#
+#class NoMoreImport(nodeTransformer.NodeTransformer):
+#  init_code = """
+#  class Module(object): pass
+#
+#  class DictModule(object):
+#    def __init__(self):
+#      self.content = {}
+#    
+#    def add(self, name, fct):
+#      self.content[name] = (False, fct)
+#
+#    def getModule(self, name):
+#      l, v = self.content[name]
+#      if not l:
+#        v = v()
+#        self.content[name] = (True, v)
+#      return v
+#      
+#  dictModule = DictModule()
+#  __import__ = dictModule.getModule
+#  """
+#
+#  def __init__(self, path):
+#    nodeTransformer.NodeTransformer.__init__(self)
+#
+#    self.resolver = SysPathFinder(path).getModuleFromName
+#    self.resolver_near = SysPathFinder(path, ['']).getModuleFromName
+#
+#    self.dict_imports = {}
+#    self.dict_imports_near = {} #the imports of files just at the same place that __file__
+#
+#
+#  def visit_withAdd(self, node):
+#    res = self.visit(node)
+#    toAdd = []
+#
+#    dict_var = self.genVar('dict_import').name
+#
+#    klassName = self.genVar('klass').name
+#
+#    toAdd = str2ast(self.init_code,
+#        DictModule = self.genVar('klass').name,
+#        dictModule = dict_var
+#    )
+#
+#
+#    for k, (vname, vast) in self.dict_imports.iteritems():
+#      toAdd += vast + str2ast("dict_var.add('%s', fct)" % k,
+#              dict_var = dict_var,
+#              fct = vname
+#      )
+#
+#    for k, (vname, vast) in self.dict_imports_near.iteritems():
+#      toAdd += vast + str2ast("dict_var.add('%s', fct)" % k,
+#              dict_var = dict_var,
+#              fct = vname
+#      )
+#
+#
+#    if isinstance(res, Module):
+#      res.body = toAdd + res.body
+#      return res
+#
+#    if isinstance(res, list):
+#      return toAdd + res
+#
+#    assert False, "type of master ast unknown"
+#
+#
+#  def visit_Import(self, node):
+#
+#    for alias in node.names:
+#      name = alias.name
+#      asname = alias.asname or name
+#
+#      #test if the name is componsed
+#      #if '.' in name:
+#      #  curName = name.split('.')[0]
+#      #  leftName = name.split('.')[1:]
+#
+#
+#      if name not in self.dict_imports_near and \
+#            name not in self.dict_imports:
+#
+#        m = self.resolver_near(name)
+#        if isinstance(m, ErrorModule):
+#          m = self.resolver(name)
+#        self.dict_imports[name] = (m.getName(), m.getAst())
+#
+#      return [
+#          str2ast("asname = __import__('%s')" % name, asname = asname),
+#      ]
+#
+#
+#
+#class SysPathFinder(object):
+#
+#  def __init__(self, path=sys.path, cur_path=os.getcwd()):
+#    self.path = sys.path
+#    self.cur_path = cur_path
+#
+#  def haveDirModule(self, name, dirPath):
+#    #test if it's have a directory module
+#    iniFile = os.path.join(dirPath, name, '__init__.py')
+#    if os.path.isfile(iniFile):
+#      return CreateModuleFromFile(iniFile, name)
+#    return None
+#
+#  def haveFileModule(self, name, dirPath):
+#    #test if it's a file module
+#    modFile = os.path.join(dirPath, name + '.py')
+#    if os.path.isfile(modFile):
+#      return CreateModuleFromFile(modFile, name)
+#    return None
+#
+#
+#  def getNameFromDir(self, name, dirPath):
+#    if dirPath == '': dirPath = self.cur_path
+#    if not os.path.isdir(dirPath): return None
+#
+#    #TODO take care of zip files
+#    return self.haveDirModule(name, dirPath) or \
+#          self.haveFileModule(name, dirPath)
+#
+#
+#  def getModuleFromName(self, name):
+#    for dirPath in self.path:
+#      m = self.getNameFromDir(name, dirPath)
+#      if m is not None: return m
+#
+#    return ErrorModule(name)
+#
+#
+#
+#class ErrorModule(nodeTransformer.VariableGenerator):
+#
+#  def __init__(self, name):
+#    self.name = name
+#
+#    self.fctName = self.genVar().name
+#
+#  def getAst(self):
+#
+#    code = """
+#      def fctName():
+#        raise ImportError("No module named %s")
+#    """ % self.name
+#
+#    return str2ast(code, fctName = self.fctName)
+#
+#  def getName(self): return self.fctName
+#
+#
+#
+#
+#
+#class CreateModuleFromFile(nodeTransformer.NodeTransformer):
+#  """
+#  Create a module object from a module file
+#  """
+#
+#
+#  def __init__(self, filePath, moduleName):
+#    self.filePath = filePath
+#    self.moduleName = moduleName
+#
+#    self.moduleFctVariable = self.genVar('moduleFct')
+#
+#    self.moduleVar = self.genVar('module')
+#
+#
+#  def getAst(self):
+#    with open(self.filePath) as f:
+#      contentAst = ast.parse(f.read(), self.filePath, 'exec')
+#    return [self.visit(contentAst)]
+#
+#  def getName(self):
+#    return self.moduleFctVariable.name
+#
+#
+#  def visit_Module(self, node):
+#
+#    args = arguments([], None, None, [])
+#
+#    init =  [
+#        self.moduleVar.assign( Call(Name('Module', Load()), [], [], None, None)  ),
+#    ]
+#
+#    res = [
+#        Return( self.moduleVar.load() ),
+#    ]
+#
+#    #add __name__ = and __file__ = in the module
+#    node.body = [
+#        Assign([Name('__name__', Store())], Str(self.moduleName)),
+#        Assign([Name('__file__', Store())], Str(self.filePath)),
+#
+#    ] + node.body
+#
+#    return FunctionDef(self.moduleFctVariable.name, args,
+#        init + self.visit(node.body) + res
+#    , [])
+#
+#  def _genAffect(self, name):
+#    return [ self.moduleVar.assign(name=name, val=Name(name, Load())), ]
+#
+#
+#  def getAllNames(self, node):
+#    if isinstance(node, Name):
+#      return [node.id]
+#
+#    if isinstance(node, List) or isinstance(node, Tuple):
+#      res = []
+#      for e in node.elts:
+#        res += self.getAllNames(e)
+#      return e
+#
+#    return []
+#
+#
+#  def visit_Assign(self, node):
+#    assert len(node.targets) == 1
+#    assert isinstance(node.targets[0], Name)
+#
+#    names = []
+#    for t in node.targets:
+#      names += self.getAllNames(t)
+#
+#    resAst = [node]
+#    for name in names:
+#      resAst += self._genAffect(name)
+#
+#    return resAst
+#
+#
+#  def visit_ClassDef(self, node):
+#    return [node] + self._genAffect(node.name)
+#
+#  def visit_FunctionDef(self, node):
+#    return [node] + self._genAffect(node.name)
+#
+#
+#
+#
+#def Resolver:
+#
+#  self.content = [] #list of dictonnary of dictionary
+#
+#
+#
+#
+#def ModuleAst:
+#
+#  self.name
+#  self.subModules = {}
+#
+#  self.filepath
+#  self.package
+#
+#
+#
+#def FileResolver:
+#  get(from = (id, name), search = name) => foundKlass
+#
+#
+#def FoundResolver:
+#  get(from = (id, name), search = name) => foundKlass
+#  __getitem__[(id, name)]
+#
+#def FoundKlass:
+#  self.moduleFound
+#  self.FoundOnTheWay
+#
+#
+#
+# - path of the current module/package
+# - path of the main path
+# - PYTHONPATH
+# - python lib
 
-  def __init__(self, path=sys.path):
-    self.path = path
-
-  def visit(self, node):
-    res = NoMoreImport(self.path).visit_withAdd(node)
-    print ast2str(res, 'pyAst_python')
-    return res
 
 
-class NoMoreImport(nodeTransformer.NodeTransformer):
-  init_code = """
-  class Module(object): pass
+#class Resolver(object):
 
-  class DictModule(object):
-    def __init__(self):
-      self.content = {}
-    
-    def add(self, name, fct):
-      self.content[name] = (False, fct)
 
-    def getModule(self, name):
-      l, v = self.content[name]
-      if not l:
-        v = v()
-        self.content[name] = (True, v)
-      return v
-      
-  dictModule = DictModule()
-  __import__ = dictModule.getModule
+
+
+
+#class Resolver
+
+
+
+
+
+
+class SimpleFileResolver(object):
+
+  def __init__(self, file_system):
+    self.fs = file_system
+
+  def setFrom(self, fromPath):
+    self.fromPath = fromPath
+    return self
+
+  def get(self, path):
+    """
+    Calcul the path from a file system
+    @return None or the complete path found
+    """
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class FileSystemFile(object):
+  """
+  Replace a file system with a dictionary-like
+  It's like a dict of dict
+
+  - To get a file, just get dict.getFile(), then open it or whatever
+  - Directory are named with a '/' at the end
   """
 
   def __init__(self, path):
-    nodeTransformer.NodeTransformer.__init__(self)
+    self.path = path
 
-    self.resolver = SysPathFinder(path).getModuleFromName
-    self.resolver_near = SysPathFinder(path, ['']).getModuleFromName
+  def __getitem__(self, name): raise KeyError("File don't have a file list")
+  def __contins__(self, name): return False
+  def __iter__(self, name): raise KeyError("File don't have a file list")
+  def __str__(self): return "'%s'" % os.path.basename(self.path)
 
-    self.dict_imports = {}
-    self.dict_imports_near = {} #the imports of files just at the same place that __file__
 
 
-  def visit_withAdd(self, node):
-    res = self.visit(node)
-    toAdd = []
+class FileSystemDir(FileSystemFile):
 
-    dict_var = self.genVar('dict_import').name
+  def __init__(self, root):
+    self.root = root
+    self.chiblings = None
 
-    klassName = self.genVar('klass').name
+  def _genChiblings(self):
+    if self.chiblings is not None: return
 
-    toAdd = str2ast(self.init_code,
-        DictModule = self.genVar('klass').name,
-        dictModule = dict_var
-    )
+    self.chiblings = {}
+    for f in os.listdir(self.root):
+      fpath = os.path.join(self.root, f)
 
+      if os.path.isfile(fpath):
+        self.chiblings[f] = FileSystemFile(fpath)
+      elif os.path.isdir(fpath):
+        self.chiblings['%s/' % f] = FileSystemDir(fpath)
+      else:
+        assert False, "what is this file %s ??" % fpath
 
-    for k, (vname, vast) in self.dict_imports.iteritems():
-      toAdd += vast + str2ast("dict_var.add('%s', fct)" % k,
-              dict_var = dict_var,
-              fct = vname
-      )
+  def __getitem__(self, name): self._genChiblings() ; return self.chiblings[name]
+  def __contains__(self, name): self._genChiblings() ;  return name in self.chiblings
+  def __iter__(self): self._genChiblings() ; return iter(self.chiblings)
 
-    for k, (vname, vast) in self.dict_imports_near.iteritems():
-      toAdd += vast + str2ast("dict_var.add('%s', fct)" % k,
-              dict_var = dict_var,
-              fct = vname
-      )
+  def __str__(self):
+    res = []
+    for name in self:
+      res.append( str(self[name]))
+    return '%s/ : {%s}' % (os.path.basename(self.root), ', '.join(res))
 
-
-    if isinstance(res, Module):
-      res.body = toAdd + res.body
-      return res
-
-    if isinstance(res, list):
-      return toAdd + res
-
-    assert False, "type of master ast unknown"
-
-
-  def visit_Import(self, node):
-
-    for alias in node.names:
-      name = alias.name
-      asname = alias.asname or name
-
-      #test if the name is componsed
-      #if '.' in name:
-      #  curName = name.split('.')[0]
-      #  leftName = name.split('.')[1:]
-
-
-      if name not in self.dict_imports_near and \
-            name not in self.dict_imports:
-
-        m = self.resolver_near(name)
-        if isinstance(m, ErrorModule):
-          m = self.resolver(name)
-        self.dict_imports[name] = (m.getName(), m.getAst())
-
-      return [
-          str2ast("asname = __import__('%s')" % name, asname = asname),
-      ]
-
-
-
-class SysPathFinder(object):
-
-  def __init__(self, path=sys.path, cur_path=os.getcwd()):
-    self.path = sys.path
-    self.cur_path = cur_path
-
-  def haveDirModule(self, name, dirPath):
-    #test if it's have a directory module
-    iniFile = os.path.join(dirPath, name, '__init__.py')
-    if os.path.isfile(iniFile):
-      return CreateModuleFromFile(iniFile, name)
-    return None
-
-  def haveFileModule(self, name, dirPath):
-    #test if it's a file module
-    modFile = os.path.join(dirPath, name + '.py')
-    if os.path.isfile(modFile):
-      return CreateModuleFromFile(modFile, name)
-    return None
-
-
-  def getNameFromDir(self, name, dirPath):
-    if dirPath == '': dirPath = self.cur_path
-    if not os.path.isdir(dirPath): return None
-
-    #TODO take care of zip files
-    return self.haveDirModule(name, dirPath) or \
-          self.haveFileModule(name, dirPath)
-
-
-  def getModuleFromName(self, name):
-    for dirPath in self.path:
-      m = self.getNameFromDir(name, dirPath)
-      if m is not None: return m
-
-    return ErrorModule(name)
-
-
-
-class ErrorModule(nodeTransformer.VariableGenerator):
-
-  def __init__(self, name):
-    self.name = name
-
-    self.fctName = self.genVar().name
-
-  def getAst(self):
-
-    code = """
-      def fctName():
-        raise ImportError("No module named %s")
-    """ % self.name
-
-    return str2ast(code, fctName = self.fctName)
-
-  def getName(self): return self.fctName
-
-
-
-
-
-class CreateModuleFromFile(nodeTransformer.NodeTransformer):
-  """
-  Create a module object from a module file
-  """
-
-
-  def __init__(self, filePath, moduleName):
-    self.filePath = filePath
-    self.moduleName = moduleName
-
-    self.moduleFctVariable = self.genVar('moduleFct')
-
-    self.moduleVar = self.genVar('module')
-
-
-  def getAst(self):
-    with open(self.filePath) as f:
-      contentAst = ast.parse(f.read(), self.filePath, 'exec')
-    return [self.visit(contentAst)]
-
-  def getName(self):
-    return self.moduleFctVariable.name
-
-
-  def visit_Module(self, node):
-
-    args = arguments([], None, None, [])
-
-    init =  [
-        self.moduleVar.assign( Call(Name('Module', Load()), [], [], None, None)  ),
-    ]
-
-    res = [
-        Return( self.moduleVar.load() ),
-    ]
-
-    #add __name__ = and __file__ = in the module
-    node.body = [
-        Assign([Name('__name__', Store())], Str(self.moduleName)),
-        Assign([Name('__file__', Store())], Str(self.filePath)),
-
-    ] + node.body
-
-    return FunctionDef(self.moduleFctVariable.name, args,
-        init + self.visit(node.body) + res
-    , [])
-
-  def _genAffect(self, name):
-    return [ self.moduleVar.assign(name=name, val=Name(name, Load())), ]
-
-
-  def getAllNames(self, node):
-    if isinstance(node, Name):
-      return [node.id]
-
-    if isinstance(node, List) or isinstance(node, Tuple):
-      res = []
-      for e in node.elts:
-        res += self.getAllNames(e)
-      return e
-
-    return []
-
-
-  def visit_Assign(self, node):
-    assert len(node.targets) == 1
-    assert isinstance(node.targets[0], Name)
-
-    names = []
-    for t in node.targets:
-      names += self.getAllNames(t)
-
-    resAst = [node]
-    for name in names:
-      resAst += self._genAffect(name)
-
-    return resAst
-
-
-  def visit_ClassDef(self, node):
-    return [node] + self._genAffect(node.name)
-
-  def visit_FunctionDef(self, node):
-    return [node] + self._genAffect(node.name)
 
 
 #__EOF__
--- script2script/test/lang/python/ast2simple/transform/test_moreImport.py
+++ script2script/test/lang/python/ast2simple/transform/test_moreImport.py
@@ -4,121 +4,196 @@ import sys
 import os.path
 
 from methodVisitUtil import MethodVisitUtil, callOnBoth, AstTransformerTestClass
-from script2script.lang.python.ast2simple.transform.moreImport import MoreImport
+from script2script.lang.python.ast2simple.transform.moreImport import *
 
-#working import
-#import failing (both for elements)
-
-print __file__
+testPath = os.path.join(os.path.dirname(__file__), 'importTest')
 
 
+class TestFileSystem(unittest.TestCase):
+
+  def test_buildFromDir(self):
+    fsd = FileSystemDir(testPath)
+
+    fsd_str = str(fsd)
+
+    waiting_for = "importTest/ : {"  \
+        "importTest_complex/ : {" \
+          "'tutu.py', 'toto.py', '__init__.py'" \
+          "}, " \
+          "importTest_first/ : {" \
+            "'import_first.py', '__init__.py'" \
+           "}, " \
+           "importTest_empty/ : {}, " \
+           "'importTest_simple.py'" \
+        "}"
+
+    self.assertEquals(fsd_str, waiting_for)
 
 
-class TestMoreImport(AstTransformerTestClass):
+
+class TestSimpleFileResolver(unittest.TestCase):
+
+  def test_simpleResolve(self):
+
+    paths = {
+       'importTest_complex/' : {
+          'tutu.py':None,
+          'toto.py':None,
+          '__init__.py':None,
+        },
+        'importTest_first/' : {
+           'import_first.py':None,
+           '__init__.py':None,
+         },
+         'importTest_empty/' : {},
+         'importTest_simple.py':None,
+    }
 
 
-  def setUp(self):
-    self.test_files = os.path.join(os.path.dirname(__file__), 'importTest')
-    self.old_path = sys.path
+    sfr = SimpleFileResolver(paths)
 
-    sys.path = [self.test_files] + sys.path
+    self.assertEquals(sfr.from('toto').get('titi'), None)
+    self.assertEquals(sfr.from('').get('titi'), None)
+    self.assertEquals(sfr.from('').get('importTest_simple'), None)
 
-  def tearDown(self):
-    sys.path = self.old_path
-
-
-  def test_Import(self):
-
-    def test_SimpleImport(m):
-      m('begin')
-      import importTest_simple
-      m(importTest_simple.v)
-      m(importTest_simple.f.__name__)
-      m(importTest_simple.c.__name__)
-      m(importTest_simple.__name__)
-      m(importTest_simple.__file__)
-      m('end')
-
-    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
-
-  def test_empty(self):
-
-    def test_empty(m):
-      m('begin')
-      try:
-        import importTest_empty
-      except ImportError:
-        m('ImportError error')
-      m('end')
-
-    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+    self.assertEquals(sfr.from('importTest_first').get('import_first'), ['importTest_first', 'import_first.py'])
+    self.assertEquals(sfr.from('').get('importTest_first.import_first'), ['importTest_first', 'import_first.py'])
+    self.assertEquals(sfr.from('importTest_first.import_first').get('import_first'), ['importTest_first', 'import_first.py'])
 
 
-  def test_directory(self):
-
-    def test_dir(m):
-      m('begin')
-      import importTest_complex
-      m(importTest_complex.v)
-      m(importTest_complex.f.__name__)
-      m(importTest_complex.c.__name__)
-      m(importTest_complex.__name__)
-      m(importTest_complex.__file__)
-      m('end')
-
-    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+    #sfr.from('toto').get('titi')
+    #sfr.from('toto').get('titi')
 
 
-  def test_first(self):
-
-    def test_first(m):
-      m('begin')
-      import importTest_first
-      m(importTest_first.a)
-      m('end')
-
-    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
 
 
-  def test_importMultiple(self):
 
-    def test_importMultiple(m):
-      m('begin')
-      import importTest_first
-      m(importTest_first.a)
-      importTest_first.a = 1
-      m(importTest_first.a)
+#   import X => ok, have a variable = module object
+#   import X.Y => ok, have a variable = module object
+#   import X.Y as titi => ok, have a variable = module object
+#   import X as titi => ok, have a variable = module object
+#   from X import * => not ok, TODO when we will have dir()
+#   from X import a, b, c => ok, variabel = module + assignation
+#   from X import a as toto => ok, variabel = module + assignation
+#   X = import('X')
+#   X = __import__('X')
 
-      import importTest_first
-      m(importTest_first.a)
-      importTest_first.a = 2
-      m(importTest_first.a)
-
-      importTest_first = __import__('importTest_first')
-      m(importTest_first.a)
-      importTest_first.a = 3
-      m(importTest_first.a)
-
-      import importTest_first as toto
-      m(importTest_first.a, toto.a)
-      importTest_first.a = 4
-      m(importTest_first.a, toto.a)
-      m('end')
-
-    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+#  from . import echo
+#  from .. import formats
+#  from ..filters import equalizer
 
 
-  def test_subImport(self):
 
-    def test_subImport(m):
-      m('begin')
 
-      #import importTest_complex.toto
-      import importTest_complex.tutu
 
-      m('end')
 
-    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+
+
+#class TestMoreImport(AstTransformerTestClass):
+#
+#
+#  def setUp(self):
+#    self.test_files = os.path.join(os.path.dirname(__file__), 'importTest')
+#    self.old_path = sys.path
+#
+#    sys.path = [self.test_files] + sys.path
+#
+#  def tearDown(self):
+#    sys.path = self.old_path
+#
+#
+#  def test_Import(self):
+#
+#    def test_SimpleImport(m):
+#      m('begin')
+#      import importTest_simple
+#      m(importTest_simple.v)
+#      m(importTest_simple.f.__name__)
+#      m(importTest_simple.c.__name__)
+#      m(importTest_simple.__name__)
+#      m(importTest_simple.__file__)
+#      m('end')
+#
+#    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+#
+#  def test_empty(self):
+#
+#    def test_empty(m):
+#      m('begin')
+#      try:
+#        import importTest_empty
+#      except ImportError:
+#        m('ImportError error')
+#      m('end')
+#
+#    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+#
+#
+#  def test_directory(self):
+#
+#    def test_dir(m):
+#      m('begin')
+#      import importTest_complex
+#      m(importTest_complex.v)
+#      m(importTest_complex.f.__name__)
+#      m(importTest_complex.c.__name__)
+#      m(importTest_complex.__name__)
+#      m(importTest_complex.__file__)
+#      m('end')
+#
+#    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+#
+#
+#  def test_first(self):
+#
+#    def test_first(m):
+#      m('begin')
+#      import importTest_first
+#      m(importTest_first.a)
+#      m('end')
+#
+#    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+#
+#
+#  def test_importMultiple(self):
+#
+#    def test_importMultiple(m):
+#      m('begin')
+#      import importTest_first
+#      m(importTest_first.a)
+#      importTest_first.a = 1
+#      m(importTest_first.a)
+#
+#      import importTest_first
+#      m(importTest_first.a)
+#      importTest_first.a = 2
+#      m(importTest_first.a)
+#
+#      importTest_first = __import__('importTest_first')
+#      m(importTest_first.a)
+#      importTest_first.a = 3
+#      m(importTest_first.a)
+#
+#      import importTest_first as toto
+#      m(importTest_first.a, toto.a)
+#      importTest_first.a = 4
+#      m(importTest_first.a, toto.a)
+#      m('end')
+#
+#    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
+#
+#
+#  def test_subImport(self):
+#
+#    def test_subImport(m):
+#      m('begin')
+#
+#      #import importTest_complex.toto
+#      import importTest_complex.tutu
+#
+#      m('end')
+#
+#    self.checkFctOnLocals(locals(), MoreImport(), mock.Mock)
 
 
 
