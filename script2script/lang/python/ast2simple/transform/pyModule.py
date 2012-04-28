32fe9028cce14472d197f62e27036fd02ce81aef
Mickael Kerbrat <stolati@gmail.com>
2012-04-28 22:47:55 +0200

    separated module from import, added dump, changed repr/str of module

---
+++ script2script/lang/python/ast2simple/transform/pyModule.py
@@ -0,0 +1,426 @@
+#!/usr/bin/env python
+
+from ast import *
+import ast, md5
+import sys
+import nodeTransformer
+import os.path, os
+import zipfile
+from nodeTransformer import str2ast, AstVariable
+
+
+#TODO puts thoses functions into script2script.tools
+def echo(fn, write=sys.stdout.write):
+  import functools
+  code = fn.func_code
+  argcount = code.co_argcount
+  argnames = code.co_varnames[:argcount]
+  fn_defaults = fn.func_defaults or list()
+  argdefs = dict(zip(argnames[-len(fn_defaults):], fn_defaults))
+
+  def format_arg_value(arg_val):
+    arg, val = arg_val
+    return '%s=%r' % (arg, val)
+
+  @functools.wraps(fn)
+  def wrapped(*v, **k):
+    positional = map(format_arg_value, zip(argnames, v))
+    defaulted = [format_arg_value((a, argdefs[a]))
+        for a in argnames[len(v):] if a not in k]
+    nameless = map(repr, v[argcount:])
+    keyword = map(format_arg_value, k.items())
+    args = positional + defaulted + nameless + keyword
+    write('%s(%s)\n' % (fn.__name__, ', '.join(args)))
+    try:
+      res = fn(*v, **k)
+      write('%s => %s' % (fn.__name__, res))
+      return res
+    except Exception as e:
+      write('%s raise %s', (fn.__name__, e))
+      raise
+
+  return wrapped
+
+
+#TODO for the future, add a list of import to include (for the __import__('name') def)
+#TODO for the future, add a list of import to not include (because they are in a if)
+#TODO do a PythonModule for .zip files
+#TODO do a test on pyc and pyo files, saying it don't take them
+
+
+class SimpleFileResolver(object):
+
+  def __init__(self, pythonModule):
+    self.pm = pythonModule
+
+  def find(self, toStr, fromStr=''):
+    """
+    helper function to get the content of the result
+    """
+    return self.getModule(self._str2names(toStr), self._str2names(fromStr))
+
+  @staticmethod
+  def _str2names(s): return s.split('.') if s else []
+
+  def getModule(self, toPath, fromPath=''):
+
+    #test from the relative path
+    if fromPath:
+      try:
+        return self.pm.getNamesRel(toPath, fromPath)
+      except NoModuleFound:
+        pass
+
+    #test from the absolute path
+    try:
+      return self.pm.getNamesAbs(toPath)
+    except NoModuleFound:
+      pass
+
+    return None
+
+
+
+class NoModuleFound(Exception): pass
+
+class PythonModule(object):
+  """
+  Represent an abstract python module
+  """
+
+  def __init__(self, name='', up=None):
+    self._name = name
+    self._up = up
+
+  def getContent(self): raise NotImplemented
+  def getChild(self, name): raise NotImplemented
+  def getChilds(self): raise NotImplemented
+  def getNamesRel(self, nameListFrom, nameListTo): raise NotImplemented
+
+  def getName(self): return self._name
+  def setName(self, name): self._name = name
+  def __hash__(self): return hash(self._name)
+
+  def __getitem__(self, name): return self.getChild(name)
+  def __iter__(self): return iter(self.getChilds())
+
+  def getUp(self): return self._up
+  def setUp(self, up): self._up = up
+
+  def getNamesAbs(self, nameList):
+    return reduce(lambda e, n: e[n] , nameList, self)
+
+  def getNames(self):
+    if self._up is None: return []
+    return self._up.getNames() + [self._name]
+
+  def __str__(self):
+    return ('%(_up)s.%(_name)s' if self._up else '%(_name)s') % self.__dict__
+
+  def __repr__(self):
+    return '<%s %s "%s">' % (self.__class__.__name__, id(self), str(self))
+
+
+  def dump(self):
+    """
+    Return a couple :
+    (hash, {})
+    - The first element is the hash of the content
+    - The second element is a dict of the childs {name : repr()}
+    """
+    hash_str = md5.new(self.getContent()).hexdigest()
+    dict_repr = {c.getName():c.dump() for c in self}
+    return (hash_str, dict_repr)
+
+
+class PythonModuleList(PythonModule):
+  """
+  Represent a list of concrete modules
+  As in python, each name is only found once (no same name at differents places)
+  So it's like a single module with childs as all the modules togethers
+  """
+
+  def __init__(self, name='', up=None):
+    PythonModule.__init__(self, name, up)
+    self._modules = [] #order is important
+    self._childs = {} #childs
+
+  def addModule(self, mod):
+    """
+    get a module
+    """
+    self._modules.append(mod)
+
+    for modChild in mod:
+      name = modChild.getName()
+      if name not in self._childs:
+        self._childs[name] = mod
+        modChild.setUp(self._up)
+
+  def getContent(self): return ''
+
+  def getChild(self, name):
+    try:
+      return self._childs[name]
+    except KeyError:
+      raise NoModuleFound()
+
+  def getChilds(self):
+    return list(self._childs.iteritems())
+
+  @echo
+  def getNamesAbs(self, nameList):
+    if not nameList: return self
+
+    curName = nameList[0]
+    if curName in self._childs:
+      return self._childs[curName].getNamesAbs(nameList[1:])
+
+    return None
+
+  def getNamesRel(self, nameListTo, nameListFrom):
+    print 'getNamesRel', nameListTo, nameListFrom
+    #TODO to remove
+    if len(nameListFrom) == 0: return self.getNamesAbs(nameListTo)
+
+    fromElement = self.getNamesAbs(nameListFrom)
+    print fromElement
+
+    if fromElement is None: return None
+
+    return fromElement.getNamesAbs(nameListTo)
+
+
+
+class PythonModuleFile(PythonModule):
+  """
+  Represent a python module on file system
+  Could be any file system, this class must be inherited
+  """
+  TYPE_ROOT, TYPE_FILE, TYPE_DIR = ('root', 'file', 'dir')
+
+  def __init__(self, up_path, name='', up=None):
+    PythonModule.__init__(self, name, up)
+
+    if self._up is None:
+      self._type, self._contentFile, self._base_dir = (self.TYPE_ROOT, None, up_path)
+      self._content = ''
+    else:
+      infos = self._findRepModule(up_path) or self._findFileModule(up_path)
+      if infos is None: raise NoModuleFound('No module with name %s in path %s' % (self._name, up_path))
+      self._type, self._contentFile, self._base_dir = infos
+      self._content = self._f_content(self._contentFile)
+
+
+  def _findRepModule(self, up_path):
+    dirPath = self._f_join(up_path, self._name)
+    filePath = self._f_join(dirPath, '__init__.py')
+    if not self._f_isfile(filePath): return None
+    return (self.TYPE_DIR, filePath, dirPath)
+
+  def _findFileModule(self, up_path):
+    filePath = self._f_join(up_path, '%s.py' % self._name)
+    if not self._f_isfile(filePath): return None
+    return (self.TYPE_FILE, filePath, up_path)
+
+  def getContent(self): return self._content
+
+  def getChild(self, name):
+    if '.' in name: raise NoModuleFound('no dots allowed in module name')
+    if self._type == self.TYPE_FILE : raise NoModuleFound('file module has no childs')
+    if name == '__init__': raise NoModuleFound('__init__ are reserved for dir elements')
+    return self._f_new(self._base_dir, name, self)
+
+  def getChilds(self):
+    if self._type == self.TYPE_FILE : return []
+    res = []
+    for name in self._f_listfiles(self._base_dir):
+      if name[-3:] == '.py': name = name[:-3]
+
+      try:
+        mod = self.getChild(name)
+      except NoModuleFound as e:
+        pass
+      else:
+        res.append(mod)
+
+    return res
+
+  def getNamesRel(self, nameListTo, nameListFrom):
+    moduleFrom = self.getNamesAbs(nameListFrom)
+    if moduleFrom._type == self.TYPE_FILE:
+      moduleFrom = moduleFrom.getUp()
+    return moduleFrom.getNamesAbs(nameListTo)
+
+  def getPath(self):
+    return self._contentFile
+
+  #Function that the class must implement
+  def _f_listfiles(self, path): raise NotImplemented
+  def _f_join(self, *args): raise NotImplemented
+  def _f_isfile(self, path): raise NotImplemented
+  def _f_isdir(self, path): raise NotImplemented
+  def _f_content(self, path): raise NotImplemented
+  def _f_new(self, base_dir, name, up): raise NotImplemented
+
+
+
+
+
+
+
+class PythonModuleOnDisk(PythonModuleFile):
+  """
+  Represent a python module on file system
+  """
+  TYPE_ROOT, TYPE_FILE, TYPE_DIR = ('root', 'file', 'dir')
+
+  def _f_listfiles(self, path):
+    return os.listdir(path)
+
+  def _f_join(self, *args): return os.path.join(*args)
+  def _f_isfile(self, path): return os.path.isfile(path)
+  def _f_isdir(self, path): return os.path.isdir(path)
+  def _f_content(self, path):
+    with open(path) as f:
+      return f.read()
+  def _f_new(self, base_dir, name, up):
+    return PythonModuleOnDisk(base_dir, name, up)
+
+
+
+
+class PythonModuleStatic(PythonModuleFile):
+
+  def __init__(self, diskContent, up_path = '', name='', up=None):
+    if up is None: #useful for test
+      up_path = name = name or up_path or '' #force if the name/up_path is not ''
+      diskContent = diskContent if name in diskContent else {name:diskContent}
+    self._diskContent = diskContent
+
+    PythonModuleFile.__init__(self, up_path, name, up)
+
+  def _s_getPathElement(self, path):
+    try:
+      return reduce(lambda r, n : r[n], path.split('/'), self._diskContent)
+    except KeyError:
+      return None
+
+  def _f_listfiles(self, path):
+    pathElem = self._s_getPathElement(path)
+    if pathElem is None : return []
+    return list(pathElem.iterkeys())
+
+  def _f_join(self, *args): return '/'.join(args)
+  def _f_isfile(self, path): return isinstance(self._s_getPathElement(path), str)
+  def _f_isdir(self, path): return isinstance(self._s_getPathElement(path), dict)
+  def _f_content(self, path): return self._s_getPathElement(path)
+  def _f_new(self, base_dir, name, up):
+    return PythonModuleStatic(self._diskContent, base_dir, name, up)
+
+
+
+
+class PythonModuleStatic(PythonModuleFile):
+  """
+  The root element should be unamed,
+  If it's have a name, it should be taken care with the upper element
+  """
+
+  def __init__(self, diskContent, up_path = '', name='', up=None):
+    if up is None: #useful for test
+      up_path = name = name or up_path or '' #force if the name/up_path is not ''
+      self._diskContent = diskContent #diskContent if name in diskContent else {name:diskContent}
+      self._root = self
+    else:
+      self._root = up._root
+      self._diskContent = diskContent
+
+    PythonModuleFile.__init__(self, up_path, name, up)
+
+  def _s_getPathElement(self, path):
+    try:
+      return reduce( lambda r, n : r[n], path.split('/')[1:], self._diskContent)
+    except KeyError:
+      return None
+
+  def _f_listfiles(self, path):
+    pathElem = self._s_getPathElement(path)
+    if pathElem is None : return []
+    return list(pathElem.iterkeys())
+
+  def _f_join(self, *args): return '/'.join(args)
+  def _f_isfile(self, path): return isinstance(self._s_getPathElement(path), str)
+  def _f_isdir(self, path): return isinstance(self._s_getPathElement(path), dict)
+  def _f_content(self, path): return self._s_getPathElement(path)
+  def _f_new(self, base_dir, name, up):
+    return PythonModuleStatic(self._diskContent, base_dir, name, up)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#TODO do for zip
+#class PythonModuleZip(PythonModuleFile):
+#
+#  def __init__(self, ziplink, up_path = '', name='', up=None):
+#    if not isinstance(ziplink, zipfile.ZipFile):
+#      up_path = os.path.basename(ziplink)[:-4]
+#      ziplink = zipfile.ZipFile(ziplink, 'r')
+#    self._zip = ziplink
+#
+#
+#    PythonModuleFile.__init__(self, up_path, name, up)
+#
+#
+#  def __del__(self):
+#    self._zip.close()
+#
+#  def _f_listfiles(self, path):
+#    res = []
+#    for name in self._zip.namelist():
+#      if name[-1] == '/' : name = name[:-1] #remove the last / char
+#      if name[:len(path)] != path: continue
+#
+#    #  if name[:len(path)] == path:
+#    #  else:
+#    #    'ko'
+#
+#    #res = []
+#    #for zipinfo in self._zip.infolist:
+#    #  fn = zipinfo.filename
+#
+#
+##
+##    return []
+##    names = [
+##      for self._zip.infolist
+#
+#  def _f_join(self, *args): return '/'.join(args)
+#
+#  def _f_isfile(self, path):
+#    return any((name == path for n in self._zip.namelist()))
+#
+#  def _f_isdir(self, path):
+#    return any((name == path+'/' for n in self._zip.namelist()))
+#
+##  def _f_content(self, path):
+##  def _f_new(self, base_dir, name, up): raise NotImplemented
+
+
+
+
+
+
+#__EOF__
---
+++ script2script/test/lang/python/ast2simple/transform/test_pyModule.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+import unittest, types, mock
+import sys
+import os.path
+
+#   import X => ok, have a variable = module object
+#   import X.Y => ok, have a variable = module object
+#   import X.Y as titi => ok, have a variable = module object
+#   import X as titi => ok, have a variable = module object
+#   from X import * => not ok, TODO when we will have dir()
+#   from X import a, b, c => ok, variabel = module + assignation
+#   from X import a as toto => ok, variabel = module + assignation
+#   X = import('X')
+#   X = __import__('X')
+
+#  from . import echo
+#  from .. import formats
+#  from ..filters import equalizer
+
+from methodVisitUtil import MethodVisitUtil, callOnBoth, AstTransformerTestClass
+from script2script.lang.python.ast2simple.transform.pyModule import *
+
+testPath = os.path.join(os.path.dirname(__file__), 'importTest')
+
+class TestPythonModuleFile(unittest.TestCase):
+
+  def test_construction(self):
+    pmf = PythonModuleOnDisk(testPath)
+
+    waiting_for = ("('d41d8cd98f00b204e9800998ecf8427e', {"
+      "'importTest_complex': ('8f1fe1ae1a4c96bf0b748ce844749ebc', {"
+        "'toto': ('8173c640b6c07b3f0d47c9a8f45875b3', {}), "
+        "'tutu': ('8f134b982b689ecfb87ac0a03c7d774c', {})"
+      "}), "
+      "'importTest_first': ('15677e4928e7fe59a4da3544371a1895', {}), "
+      "'importTest_simple': ('3a0008caa56ac47f82d9d19e07473efa', {})"
+    "})")
+
+    self.assertEquals(repr(pmf.dump()), waiting_for)
+
+
+class TestPythonModuleStatic(unittest.TestCase):
+
+  def test_construction(self):
+    paths = {
+      'importTest_complex' : {
+        'tutu.py': 'importTest_complex/tutu.py',
+        'toto.py': 'importTest_complex/toto.py',
+        '__init__.py': 'importTest_complex/__init__.py',
+      },
+      'importTest_first' :  {
+        '__init__.py': 'importTest_first/__init__.py',
+      },
+      'importTest_first.py' : 'importTest_first/import_first.py',
+      'importTest_empty': {},
+      'importTest_simple.py': 'importTest_simple.py',
+    }
+
+    pms = PythonModuleStatic(paths)
+
+    waiting_for = ("('d41d8cd98f00b204e9800998ecf8427e', {"
+      "'importTest_complex': ('9679640927855358ac46d17a76a10b5d', {"
+        "'toto': ('266020d79799fa1356744ff4af5d4869', {}), "
+        "'tutu': ('dc97b1a2e8269293e02aed087ff39cdc', {})"
+      "}), "
+      "'importTest_first': ('49c275c5921913e3c3fe90f08de0f9b7', {}), "
+      "'importTest_simple': ('d5b1e8321944d1377924dd34bfbadf3d', {})"
+    "})")
+
+    self.assertEquals(repr(pms.dump()), waiting_for)
+
+
+class TestSimpleFileResolver(unittest.TestCase):
+
+  def setUp(self):
+    self.paths = {
+      'importTest_complex' : {
+        'tutu.py': 'importTest_complex/tutu.py',
+        'toto.py': 'importTest_complex/toto.py',
+        '__init__.py': 'importTest_complex/__init__.py',
+        'tyty': {
+          'trala.py': 'importTest_complex/tyty/trala.py',
+          '__init__.py': 'importTest_complex/tyty/__init__.py',
+        },
+      },
+      'importTest_first' :  {
+        'import_first.py' : 'importTest_first/import_first.py',
+        '__init__.py': 'importTest_first/__init__.py',
+      },
+      'imporTest_empty': {},
+      'importTest_simple.py': 'importTest_simple.py',
+    }
+
+
+  def test_simpleResolver(self):
+
+    testVals = [
+      #abs path
+      (('toto', ''), None),
+      (('importTest_simple', ''), 'importTest_simple.py'),
+      (('importTest_complex', ''), 'importTest_complex/__init__.py'),
+      (('importTest_complex.tutu', ''), 'importTest_complex/tutu.py'),
+
+      (('toto', 'importTest_first'), None),
+      (('importTest_simple', 'importTest_first'), 'importTest_simple.py'),
+      (('importTest_complex', 'importTest_first'), 'importTest_complex/__init__.py'),
+      (('importTest_complex.tutu', 'importTest_first'), 'importTest_complex/tutu.py'),
+
+      #rel path
+      (('import_first', 'importTest_first'), 'importTest_first/import_first.py'),
+      (('tutu', 'importTest_complex'), 'importTest_complex/tutu.py'),
+      (('tutu', 'importTest_complex.toto'), 'importTest_complex/tutu.py'),
+      (('toto', 'importTest_complex.tutu'), 'importTest_complex/toto.py'),
+      (('tyty.trala', 'importTest_complex.toto'), 'importTest_complex/tyty/trala.py'),
+    ]
+
+    pms = PythonModuleStatic(self.paths)
+    sfr = SimpleFileResolver(pms)
+
+    for (strTo, strFrom), resWait in testVals:
+      m = sfr.find(strTo, strFrom)
+      resReal = m.getContent() if m is not None else None
+      self.assertEquals(resWait, resReal)
+
+
+
+class TestMultipleModuleList(unittest.TestCase):
+
+  def setUp(self):
+    self.test_static = [
+      {
+        'a1' : {
+          'a.py': 'a1/a.py',
+          'b.py': 'a1/b.py',
+          '__init__.py': 'a1/__init__.py',
+          'c': {
+            'a.py': 'a/c/a.py',
+            '__init__.py': 'a/c/__init__.py',
+          },
+        },
+        'a2' :  {
+          'a.py' : 'a2/a.py',
+          '__init__.py': 'a2/__init__.py',
+        },
+        'a3.py': 'a3.py',
+      },
+      {
+        'a4' : {
+          'a.py': 'a4/a.py',
+          'b.py': 'a4/b.py',
+          '__init__.py': 'a4/__init__.py',
+          'c': {
+            'a.py': 'a4/c/a.py',
+            '__init__.py': 'a4/c/__init__.py',
+          },
+        },
+        'a5' :  {
+          'a.py' : 'a5/a.py',
+          '__init__.py': 'a5/__init__.py',
+        },
+        'a6.py': 'a6.py',
+      }
+    ]
+
+
+    place = os.path.join(testPath, 'onModule')
+
+    self.test_ondisk = [
+      os.path.join(place, 'importA'),
+      os.path.join(place, 'importB'),
+    ]
+
+    self.test_zip = [
+      os.path.join(place, 'importA.zip'),
+      os.path.join(place, 'importB.zip'),
+    ]
+
+    self.testVals = [
+      #(('a1', ''), ('a1/__init__.py', 'a1')), #load a module dir
+      #(('a1.a', ''), ('a1/a.py', 'a1.a')), #load a module file
+      #(('a4', 'a1'), ('a4/__init__.py', 'a4')), #load a dir module from the other side
+      #(('a4.a', 'a1'), ('a4/a.py', 'a4.a')), #load a file module from the other side
+      #(('a', 'a1'), ('a1/a.py', 'a1.a')), #load a file from inside
+      #(('c', 'a1'), ('a1/c/__init__.py', 'a1.c')), #load a dir from inside
+      #(('c.a', 'a1'), ('a1/c/a.py', 'a1.c.a')), #load a dir/file from inside
+      #(('a1.c.a', ''), ('a1/c/a.py', 'a1.c.a')), #load a dir/file from base
+      #(('a1.c.a', 'a4.c.a'), ('a1/c/a.py', 'a1.c.a')), #load a dir/file from other side
+      #(('a', 'a1.c'), ('a1/c/a.py', 'a1.c.a')), #acces 'a' from a stuff
+      #(('a', 'a2'), ('a2/a.py', 'a2.a')), #acess 'a' from another stuff
+
+      ##same as previous, the other way around
+      #(('a4', ''), ('a4/__init__.py', 'a4')), #load a module dir
+      #(('a4.a', ''), ('a4/a.py', 'a4.a')), #load a module file
+      #(('a1', 'a4'), ('a1/__init__.py', 'a1')), #load a dir module from the other side
+      #(('a1.a', 'a4'), ('a1/a.py', 'a1.a')), #load a file module from the other side
+      #(('a', 'a4'), ('a4/a.py', 'a4.a')), #load a file from inside
+      #(('c', 'a4'), ('a4/c/__init__.py', 'a4.c')), #load a dir from inside
+      #(('c.a', 'a4'), ('a4/c/a.py', 'a4.c.a')), #load a dir/file from inside
+      #(('a4.c.a', ''), ('a4/c/a.py', 'a4.c.a')), #load a dir/file from base
+      #(('a4.c.a', 'a1.c.a'), ('a4/c/a.py', 'a4.c.a')), #load a dir/file from other side
+      #(('a', 'a4.c'), ('a4/c/a.py', 'a4.c.a')), #acces 'a' from a stuff
+      #(('a', 'a5'), ('a5/a.py', 'a5.a')), #acess 'a' from another stuff
+    ]
+
+
+  def loopTestsOn(self, (moduleAFct, resAFct), (moduleBFct, resBFct)):
+
+    pml = PythonModuleList()
+
+    pml.addModule(moduleAFct)
+    pml.addModule(moduleBFct)
+
+    sfr = SimpleFileResolver(pml)
+
+    for (toStr, fromStr), (pathRes, nameRes) in self.testVals:
+      res = sfr.find(toStr, fromStr)
+      if res is None:
+        pathResReal, nameResReal = (None, None)
+      else:
+        pathResReal, nameResReal = res.getPath(), '.'.join(res.getNames())
+        pathResReal = resAFct(resBFct(pathResReal))
+
+      self.assertEquals((pathRes, nameRes), (pathResReal, nameResReal))
+
+
+  def testStaticStatic(self):
+
+    staticNewA = PythonModuleStatic(self.test_static[0])
+    staticResA = lambda x:x
+    onDiskNewA = PythonModuleOnDisk(self.test_ondisk[0])
+    onDiskResA = lambda s : s.replace(self.test_ondisk[0], '0')
+
+    staticNewB = PythonModuleStatic(self.test_static[1])
+    staticResB = lambda x:x
+    onDiskNewB = PythonModuleOnDisk(self.test_ondisk[1])
+    onDiskResB = lambda s : s.replace(self.test_ondisk[1], '1')
+
+    As = [(staticNewA, staticResA), ] #(onDiskNewA, onDiskResA)]
+    Bs = [(staticNewB, staticResB), ] #(onDiskNewB, onDiskResB)]
+
+    for a in As:
+      for b in Bs:
+        self.loopTestsOn(a, b)
+
+
+
+if __name__ == "__main__":
+  unittest.main()
+
+
+
+#__EOF__
